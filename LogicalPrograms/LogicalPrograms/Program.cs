d8841c27981564aab030e85aca3bf7be012b38ee
sahiba <sahiba1214@gmail.com>
2021-11-18 15:40:02 +0530

    [sahiba1214] add : file added

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -4,26 +4,28 @@ using System.Text;
 
 namespace LogicalPrograms
 {
-    public class FibonacciSeries
+    public class PerfectNumber
     {
-        public void Fibonacci()
+        public void Perfect()
         {
-            int first = 0, second = 1, third = 0;
-            Console.WriteLine("Enter the number of series :");
-            int series = Convert.ToInt32(Console.ReadLine());
-            if (series <= 1)
+            int sum = 0;
+            Console.WriteLine("Enter the number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
             {
-                Console.WriteLine(first);
-                return;
+                if (num % i == 0)
+                {
+                    sum += i;
+                    Console.WriteLine("{0} ", i);
+                }
             }
-            Console.WriteLine(first);
-            Console.WriteLine(second);
-            for (int i = 2; i < series; i++)
+            if (sum == num)
             {
-                third = first + second;
-                first = second;
-                second = third;
-                Console.WriteLine(third);
+                Console.WriteLine(num + " is a Perfect number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not a Perfect number");
             }
         }
     }
